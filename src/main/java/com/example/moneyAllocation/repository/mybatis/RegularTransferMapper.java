bd0d237e633429ffea9e9232474a5737f156e733
ryota8128 <ryota2339@icloud.com>
2023-07-04 00:17:55 +0900

    feat: mapper作成

---
+++ src/main/java/com/example/moneyAllocation/repository/mybatis/RegularTransferMapper.java
@@ -0,0 +1,18 @@
+package com.example.moneyAllocation.repository.mybatis;
+
+import com.example.moneyAllocation.domain.RegularTransfer;
+import com.example.moneyAllocation.domain.RegularTransferSelector;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface RegularTransferMapper {
+    List<RegularTransfer> find(RegularTransferSelector selector);
+
+    int add(RegularTransfer regularTransfer);
+
+    int set(RegularTransfer regularTransfer);
+
+    int delete(@Param("id") Long id);
+}
