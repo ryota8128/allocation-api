82bfdc7886a958e528501515de22c20bc8d70de8
ryota8128 <ryota2339@icloud.com>
2023-07-05 01:07:49 +0900

    feat: TemporaryTransferController作成

---
+++ src/main/java/com/example/moneyAllocation/controller/TemporaryTransferController.java
@@ -0,0 +1,50 @@
+package com.example.moneyAllocation.controller;
+
+import com.example.moneyAllocation.domain.TemporaryTransfer;
+import com.example.moneyAllocation.domain.TemporaryTransferSelector;
+import com.example.moneyAllocation.service.TemporaryTransferService;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/temporary")
+public class TemporaryTransferController {
+    private final TemporaryTransferService service;
+
+    public TemporaryTransferController(TemporaryTransferService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<TemporaryTransfer> find(@RequestParam(required = false) Long userId) {
+        TemporaryTransferSelector selector = new TemporaryTransferSelector();
+        selector.setUserId(userId);
+        return service.find(selector);
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody TemporaryTransfer temporaryTransfer) {
+        service.add(temporaryTransfer);
+    }
+
+    @PatchMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void set(@RequestBody TemporaryTransfer temporaryTransfer) {
+        service.set(temporaryTransfer);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+
+}
