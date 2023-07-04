20e0794cd0fbf5eaabaec53b66d33060583b3be5
ryota8128 <ryota2339@icloud.com>
2023-07-05 00:59:02 +0900

    feat: TemporaryTransferRepository作成

---
+++ src/main/java/com/example/moneyAllocation/repository/TemporaryTransferRepository.java
@@ -0,0 +1,15 @@
+package com.example.moneyAllocation.repository;
+
+import com.example.moneyAllocation.domain.TemporaryTransfer;
+import com.example.moneyAllocation.domain.TemporaryTransferSelector;
+import java.util.List;
+
+public interface TemporaryTransferRepository {
+    List<TemporaryTransfer> find(TemporaryTransferSelector selector);
+
+    void add(TemporaryTransfer temporaryTransfer);
+
+    void set(TemporaryTransfer temporaryTransfer);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com/example/moneyAllocation/repository/TemporaryTransferRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.example.moneyAllocation.repository;
+
+import com.example.moneyAllocation.domain.TemporaryTransfer;
+import com.example.moneyAllocation.domain.TemporaryTransferSelector;
+import com.example.moneyAllocation.repository.mybatis.TemporaryTransferMapper;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+
+public class TemporaryTransferRepositoryImpl implements TemporaryTransferRepository{
+
+    private final SqlSession sqlSession;
+
+    public TemporaryTransferRepositoryImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    @Override
+    public List<TemporaryTransfer> find(TemporaryTransferSelector selector) {
+        return sqlSession.getMapper(TemporaryTransferMapper.class).find(selector);
+    }
+    @Override
+    public void add(TemporaryTransfer temporaryTransfer) {
+        int affected = sqlSession.getMapper(TemporaryTransferMapper.class).add(temporaryTransfer);
+        if (affected != 1) {
+            throw new RuntimeException("データの追加に失敗しました．");
+        }
+    }
+
+    @Override
+    public void set(TemporaryTransfer temporaryTransfer) {
+        int affected = sqlSession.getMapper(TemporaryTransferMapper.class).set(temporaryTransfer);
+        if (affected != 1) {
+            throw new ResourceNotFoundException("TemporaryTransfer not found");
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        int affected = sqlSession.getMapper(TemporaryTransferMapper.class).delete(id);
+        if (affected != 1) {
+            throw new ResourceNotFoundException("TemporaryTransfer not found");
+        }
+
+
+    }
+}
