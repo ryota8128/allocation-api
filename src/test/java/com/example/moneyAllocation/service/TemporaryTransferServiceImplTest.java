f931c6d71d1dca462841ec796206619a97a92bf3
ryota8128 <ryota2339@icloud.com>
2023-07-05 22:40:49 +0900

    test: TemporaryTransferServiceImplTest追加

---
+++ src/test/java/com/example/moneyAllocation/service/TemporaryTransferServiceImplTest.java
@@ -0,0 +1,70 @@
+package com.example.moneyAllocation.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.example.moneyAllocation.domain.TemporaryTransfer;
+import com.example.moneyAllocation.domain.TemporaryTransferSelector;
+import com.example.moneyAllocation.repository.TemporaryTransferRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+class TemporaryTransferServiceImplTest {
+    @Mock
+    private TemporaryTransferRepository repository;
+
+    @InjectMocks
+    private TemporaryTransferServiceImpl service;
+
+    private AutoCloseable mocks;
+
+    @BeforeEach
+    public void before() {
+        mocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    public void after() throws Exception {
+        mocks.close();
+    }
+
+    @Test
+    void find() {
+        TemporaryTransferSelector selector = new TemporaryTransferSelector();
+        List<TemporaryTransfer> temporaryTransferList = new ArrayList<>();
+        temporaryTransferList.add(new TemporaryTransfer());
+        Mockito.doReturn(temporaryTransferList).when(repository).find(selector);
+        List<TemporaryTransfer> result = service.find(selector);
+        assertEquals(result, temporaryTransferList);
+        Mockito.verify(repository, Mockito.times(1)).find(selector);
+    }
+
+    @Test
+    void add() {
+        TemporaryTransfer temporaryTransfer = new TemporaryTransfer();
+        Mockito.doNothing().when(repository).add(temporaryTransfer);
+        service.add(temporaryTransfer);
+        Mockito.verify(repository, Mockito.times(1)).add(temporaryTransfer);
+    }
+
+    @Test
+    void set() {
+        TemporaryTransfer temporaryTransfer = new TemporaryTransfer();
+        Mockito.doNothing().when(repository).set(temporaryTransfer);
+        service.set(temporaryTransfer);
+        Mockito.verify(repository, Mockito.times(1)).set(temporaryTransfer);
+    }
+
+    @Test
+    void delete() {
+        Long id = 2L;
+        Mockito.doNothing().when(repository).delete(id);
+        service.delete(id);
+        Mockito.verify(repository, Mockito.times(1)).delete(id);
+    }
+}
