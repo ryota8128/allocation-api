34569f6ae71679a9f866687ea5c8ef892668a57c
ryota8128 <ryota2339@icloud.com>
2023-07-08 17:53:53 +0900

    fix: cors設定

--- src/main/java/com/example/moneyAllocation/config/SecurityConfig.java
+++ src/main/java/com/example/moneyAllocation/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
