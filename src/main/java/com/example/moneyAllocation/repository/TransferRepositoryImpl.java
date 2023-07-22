fba32f84fb2afa92c1e24b607c5f930153f31cd1
ryota8128 <ryota2339@icloud.com>
2023-07-22 21:16:47 +0900

    feat: repositoryImpl実装

--- src/main/java/com/example/moneyAllocation/repository/TransferRepositoryImpl.java
+++ src/main/java/com/example/moneyAllocation/repository/TransferRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.example.moneyAllocation.repository;
 
 import com.example.moneyAllocation.domain.Transfer;
 import com.example.moneyAllocation.domain.TransferSelector;
+import com.example.moneyAllocation.exception.ResourceNotFoundException;
 import com.example.moneyAllocation.repository.mybatis.TransferMapper;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
@@ -22,21 +23,35 @@ public class TransferRepositoryImpl implements TransferRepository {
 
     @Override
     public Transfer findOne(TransferSelector selector) {
-        return sqlSession.getMapper(TransferMapper.class).findOne(selector);
+        Transfer transfer = sqlSession.getMapper(TransferMapper.class).findOne(selector);
+        if (transfer == null) {
+            throw  new ResourceNotFoundException("Transfer not found");
+        }
+        return transfer;
     }
 
     @Override
     public void add(Transfer transfer) {
-
+        int affected = sqlSession.getMapper(TransferMapper.class).add(transfer);
+        if (affected != 1) {
+            throw new RuntimeException("データの追加に失敗しました");
+        }
     }
 
     @Override
     public void set(Transfer transfer) {
+        int affected = sqlSession.getMapper(TransferMapper.class).add(transfer);
+        if (affected != 1) {
+            throw new ResourceNotFoundException("Transfer not found");
+        }
 
     }
 
     @Override
     public void delete(TransferSelector selector) {
-
+        int affected = sqlSession.getMapper(TransferMapper.class).delete(selector);
+        if (affected != 1) {
+            throw new ResourceNotFoundException("Transfer not found");
+        }
     }
 }
