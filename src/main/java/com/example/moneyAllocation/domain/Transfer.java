3c6cb72d484e76d8fe1ec77db60cdcddccb31dd9
ryota8128 <ryota2339@icloud.com>
2023-07-22 20:03:13 +0900

    feat: Transferドメイン作成

---
+++ src/main/java/com/example/moneyAllocation/domain/Transfer.java
@@ -0,0 +1,33 @@
+package com.example.moneyAllocation.domain;
+
+public class Transfer {
+    private Long id;
+
+    private String title;
+
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
