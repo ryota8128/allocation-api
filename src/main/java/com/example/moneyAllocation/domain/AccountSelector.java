4822505321bea4a6cb09b492703a719d4db00a36
ryota8128 <ryota2339@icloud.com>
2023-07-29 15:14:56 +0900

    feat: AccountSelector変更

--- src/main/java/com/example/moneyAllocation/domain/AccountSelector.java
+++ src/main/java/com/example/moneyAllocation/domain/AccountSelector.java
@@ -5,6 +5,16 @@ public class AccountSelector {
 
     private Long id;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
