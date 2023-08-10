e9325fbcb2df434f53d08d47b4d0a6e7199ba161
ryota8128 <ryota2339@icloud.com>
2023-08-10 23:10:52 +0900

    test: AccountRepositoryImplTest修正

--- src/test/java/com/example/moneyAllocation/repository/AccountRepositoryImplTest.java
+++ src/test/java/com/example/moneyAllocation/repository/AccountRepositoryImplTest.java
@@ -17,124 +17,129 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 class AccountRepositoryImplTest {
-    @Mock
-    SqlSession sqlSession;
+  @Mock SqlSession sqlSession;
 
-    @Mock
-    AccountMapper mapper;
+  @Mock AccountMapper mapper;
 
-    @Mock
-    RegularTransferRepository regularTransferRepository;
+  @Mock RegularTransferRepository regularTransferRepository;
 
-    @Mock
-    TemporaryTransferRepository temporaryTransferRepository;
+  @Mock TemporaryTransferRepository temporaryTransferRepository;
 
-    @InjectMocks
-    private AccountRepositoryImpl repository;
+  @InjectMocks @Spy private AccountRepositoryImpl repository;
 
-    @BeforeEach
-    public void before() {
-        MockitoAnnotations.openMocks(this);
-        Mockito.doReturn(mapper).when(sqlSession).getMapper(AccountMapper.class);
-    }
+  @BeforeEach
+  public void before() {
+    MockitoAnnotations.openMocks(this);
+    Mockito.doReturn(mapper).when(sqlSession).getMapper(AccountMapper.class);
+  }
 
-    @AfterEach
-    public void after() {
-        Mockito.verify(sqlSession, Mockito.times(1)).getMapper(AccountMapper.class);
-    }
+  @AfterEach
+  public void after() {
+    Mockito.verify(sqlSession, Mockito.times(1)).getMapper(AccountMapper.class);
+  }
 
-    @Test
-    void find() {
-        Long ownerId = 2L;
-        List<Account> accountList = new ArrayList<>();
-        accountList.add(new Account());
+  @Test
+  void find() {
+    Long ownerId = 2L;
+    List<Account> accountList = new ArrayList<>();
+    accountList.add(new Account());
 
-        Mockito.doReturn(accountList).when(mapper).find(ownerId);
+    Mockito.doReturn(accountList).when(mapper).find(ownerId);
 
-        List<Account> result = this.repository.find(ownerId);
-        assertEquals(accountList, result);
-        Mockito.verify(mapper, Mockito.times(1)).find(ownerId);
-    }
+    List<Account> result = this.repository.find(ownerId);
+    assertEquals(accountList, result);
+    Mockito.verify(mapper, Mockito.times(1)).find(ownerId);
+  }
 
-    @Test
-    void findOne() {
-        Account account = new Account();
-        AccountSelector selector = new AccountSelector();
-        selector.setId(1L);
-        selector.setOwnerId(1L);
+  @Test
+  void findOne() {
+    Account account = new Account();
+    AccountSelector selector = new AccountSelector();
+    selector.setId(1L);
+    selector.setOwnerId(1L);
 
-        Mockito.doReturn(account).when(mapper).findOne(selector);
-        Account result = repository.findOne(selector);
-        assertEquals(account, result);
-        Mockito.verify(mapper, Mockito.times(1)).findOne(selector);
-    }
+    Mockito.doReturn(account).when(mapper).findOne(selector);
+    Account result = repository.findOne(selector);
+    assertEquals(account, result);
+    Mockito.verify(mapper, Mockito.times(1)).findOne(selector);
+  }
 
-    @Test
-    void findOneNotExists() {
-        AccountSelector selector = new AccountSelector();
-        selector.setId(1L);
-        selector.setOwnerId(1L);
-        Mockito.doReturn(null).when(mapper).findOne(selector);
-        assertThrows(ResourceNotFoundException.class, () -> repository.findOne(selector));
-        Mockito.verify(mapper, Mockito.times(1)).findOne(selector);
-    }
+  @Test
+  void findOneNotExists() {
+    AccountSelector selector = new AccountSelector();
+    selector.setId(1L);
+    selector.setOwnerId(1L);
+    Mockito.doReturn(null).when(mapper).findOne(selector);
+    assertThrows(ResourceNotFoundException.class, () -> repository.findOne(selector));
+    Mockito.verify(mapper, Mockito.times(1)).findOne(selector);
+  }
 
-    @Test
-    void add() {
-        Account account = new Account();
-        Mockito.doReturn(1).when(mapper).add(account);
-        this.repository.add(account);
+  @Test
+  void add() {
+    Account account = new Account();
+    Mockito.doReturn(1).when(mapper).add(account);
+    this.repository.add(account);
 
-        Mockito.verify(mapper, Mockito.times(1)).add(account);
-    }
+    Mockito.verify(mapper, Mockito.times(1)).add(account);
+  }
 
-    @Test
-    void addFail() {
-        Account account = new Account();
-        Mockito.doReturn(0).when(mapper).add(account);
-        assertThrowsExactly(RuntimeException.class, () -> this.repository.add(account));
-        Mockito.verify(mapper, Mockito.times(1)).add(account);
-    }
+  @Test
+  void addFail() {
+    Account account = new Account();
+    Mockito.doReturn(0).when(mapper).add(account);
+    assertThrowsExactly(RuntimeException.class, () -> this.repository.add(account));
+    Mockito.verify(mapper, Mockito.times(1)).add(account);
+  }
 
+  @Test
+  void set() {
+    Account account = new Account();
+    Mockito.doReturn(1).when(mapper).set(account);
+    this.repository.set(account);
+    Mockito.verify(mapper, Mockito.times(1)).set(account);
+  }
 
-    @Test
-    void set() {
-        Account account = new Account();
-        Mockito.doReturn(1).when(mapper).set(account);
-        this.repository.set(account);
-        Mockito.verify(mapper, Mockito.times(1)).set(account);
-    }
+  @Test
+  void setFail() {
+    Account account = new Account();
+    Mockito.doReturn(0).when(mapper).set(account);
+    assertThrowsExactly(ResourceNotFoundException.class, () -> this.repository.set(account));
+    Mockito.verify(mapper, Mockito.times(1)).set(account);
+  }
 
-    @Test
-    void setFail() {
-        Account account = new Account();
-        Mockito.doReturn(0).when(mapper).set(account);
-        assertThrowsExactly(ResourceNotFoundException.class, () -> this.repository.set(account));
-        Mockito.verify(mapper, Mockito.times(1)).set(account);
-    }
+  @Test
+  void delete() {
+    AccountSelector selector = new AccountSelector();
+    selector.setId(1L);
+    selector.setOwnerId(2L);
+    Mockito.doReturn(1).when(mapper).delete(selector);
+    Mockito.doNothing()
+        .when(repository)
+        .setNullToViaThatReferenceDeleteAccount(selector.getOwnerId(), selector.getId());
+    Mockito.doNothing().when(regularTransferRepository).setNullAccount(Mockito.eq(1L));
+    Mockito.doNothing().when(temporaryTransferRepository).setNullAccount(Mockito.eq(1L));
 
+    this.repository.delete(selector);
+    Mockito.verify(mapper, Mockito.times(1)).delete(selector);
+    Mockito.verify(regularTransferRepository, Mockito.times(1)).setNullAccount(Mockito.eq(1L));
+    Mockito.verify(temporaryTransferRepository, Mockito.times(1)).setNullAccount(Mockito.eq(1L));
+    Mockito.verify(repository, Mockito.times(1))
+        .setNullToViaThatReferenceDeleteAccount(selector.getOwnerId(), selector.getId());
+  }
 
-    @Test
-    void delete() {
-        AccountSelector selector = new AccountSelector();
-        selector.setId(1L);
-        Mockito.doReturn(1).when(mapper).delete(selector);
-        Mockito.doNothing().when(regularTransferRepository).setNullAccount(Mockito.eq(1L));
-        Mockito.doNothing().when(temporaryTransferRepository).setNullAccount(Mockito.eq(1L));
-
-        this.repository.delete(selector);
-        Mockito.verify(mapper, Mockito.times(1)).delete(selector);
-        Mockito.verify(regularTransferRepository, Mockito.times(1)).setNullAccount(Mockito.eq(1L));
-        Mockito.verify(temporaryTransferRepository, Mockito.times(1)).setNullAccount(Mockito.eq(1L));
-    }
-
-    @Test
-    void deleteFail() {
-        AccountSelector selector = new AccountSelector();
-        Mockito.doReturn(0).when(mapper).delete(selector);
-        assertThrowsExactly(ResourceNotFoundException.class, () -> this.repository.delete(selector));
-        Mockito.verify(mapper, Mockito.times(1)).delete(selector);
-    }
+  @Test
+  void deleteFail() {
+    AccountSelector selector = new AccountSelector();
+    selector.setId(1L);
+    selector.setOwnerId(2L);
+    Mockito.doReturn(0).when(mapper).delete(selector);
+    Mockito.doNothing()
+        .when(repository)
+        .setNullToViaThatReferenceDeleteAccount(selector.getOwnerId(), selector.getId());
+    assertThrowsExactly(ResourceNotFoundException.class, () -> this.repository.delete(selector));
+    Mockito.verify(mapper, Mockito.times(1)).delete(selector);
+  }
 }
